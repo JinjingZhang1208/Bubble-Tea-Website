1a9371d7c6fa8fc20bf0b7006811b2321ebaaa98
Jinjing Zhang <127969588+JinjingZhang1208@users.noreply.github.com>
2023-12-03 12:51:08 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,40 +24,6 @@ app.get('/api/menuItems', async (req, res) => {
   }
 });
 
-// User registration endpoint
-app.post('/api/register', async (req, res) => {
-  const { auth0Id, name, email, emailVerified, picture } = req.body;
-
-  try {
-    await prisma.user.create({
-      data: { auth0Id, name, email, emailVerified, picture },
-    });
-
-    res.status(201).send('User registered successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error registering user');
-  }
-});
-
-// Fetch user information endpoint
-app.get('/api/user/:id', async (req, res) => {
-  const userId = Number(req.params.id);
-
-  try {
-    const user = await prisma.user.findUnique({ where: { id: userId } });
-
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).send('User not found');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching user information');
-  }
-});
-
 // Start the HTTP server
 app.listen(8000, () => {
   console.log('Server running on http://localhost:8000 🎉 🚀');
