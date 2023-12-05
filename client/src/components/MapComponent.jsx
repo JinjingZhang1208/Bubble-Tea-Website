4204a19143ee608030e73742816317488b44cb33
Jinjing Zhang <127969588+JinjingZhang1208@users.noreply.github.com>
2023-12-05 12:09:26 -0800

    update

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -2,28 +2,49 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 class MapComponent extends Component {
+  state = {
+    userLocation: null,
+  };
+
+  componentDidMount() {
+    this.getUserLocation();
+  }
+
+  getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          this.setState({ userLocation });
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  };
+
   render() {
+    const { google } = this.props;
+    const { userLocation } = this.state;
+    const storeLocation = { lat: 49.2827, lng: -123.1207 };
+
     const mapStyles = {
       width: '80%',
       height: '400px',
     };
 
-    const location = { lat: 49.2827, lng: -123.1207 };
-    const googleMapsUrl = `https://www.google.com/maps?q=${location.lat},${location.lng}`;
-
     return (
       <div>
-        <Map
-          google={this.props.google}
-          zoom={15}
-          style={mapStyles}
-          initialCenter={location}
-        >
-          <Marker position={location} />
+        <Map google={google} zoom={15} style={mapStyles} initialCenter={storeLocation}>
+          {userLocation && <Marker position={userLocation} label="You are here" />}
+          <Marker position={storeLocation} label="Store" />
         </Map>
-        <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
-          Open in Google Maps
-        </a>
       </div>
     );
   }
@@ -32,3 +53,5 @@ class MapComponent extends Component {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyA7_OuH5C2ADYpIjc3dqFy7wxsCKMD8eAI',
 })(MapComponent);
+
+
